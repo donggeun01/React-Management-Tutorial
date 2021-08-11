4003ec246668ce0693a07ac1bebb53beaa7d7f12
dong <ehdrms1153@gmail.com>
2021-08-11 11:38:10 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead'
 import TableBody from '@material-ui/core/TableBody'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,28 +18,48 @@ const styles = theme => ({
     },
     table: {
         minWidth: 1080
+    },
+    progress: {
+        margin: theme.spacing(2)
     }
 })
+
+
+/*
+
+- React 라이프 사이클 -
+
+1. constructor()
+
+2. componentWillMount()
+
+3. render()
+
+4. componentDidMount()
+
+ */
 
 
 // map 사용 시 key props 필요
 class App extends Component {
     state = {
-        customers: ""
+        customers: "",
+        completed: 0
     }
     componentDidMount() {
+        this.timer = setInterval(this.progress, 20);
         this.callApi()
-            .then(res => {
-                this.setState({customers: res})
-                console.log(this.state.customers)
-            })
+            .then(res => this.setState({customers: res}))
             .catch(err => console.log(err));
     }
-
     callApi = async () => {
         const response = await fetch('/api/customers')
         const body = await response.json();
         return body
+    }
+    progress = () => {
+        const { completed } = this.state;
+        this.setState({completed: completed >= 100 ? 0: completed + 1});
     }
 
     render() {
@@ -60,7 +81,13 @@ class App extends Component {
                     <TableBody>
                         { this.state.customers ? this.state.customers.map(c => {
                             return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)
-                        }) : "" }
+                        }) :
+                        <TableRow>
+                            <TableCell colSpan="6" align="center">
+                                <CircularProgress className={classes.progress}></CircularProgress>
+                            </TableCell>
+                        </TableRow>
+                        }
                     </TableBody>
                 </Table>
 
